2990490ed74fa0e02100df27d4be23edc56a7174
{DanteWeverbergh} <{dantweve@student.arteveldehs.be}>
2022-11-16 11:01:43 +0100

    :rocket: A/B testing

---
+++ script.js
@@ -0,0 +1,36 @@
+//script
+
+//get querystring
+
+const params = new URLSearchParams(window.location.search);
+
+if (params.has('version')) {
+  if (params.get('version') === 'a') {
+    version = 'a';
+  } else if (params.get('version') === 'b') {
+    version = 'b';
+  }
+} else {
+  version = 'a';
+}
+
+//elemnts
+const title = document.getElementById('title');
+const image = document.getElementById('image');
+const button = document.querySelector('button');
+const body = document.querySelector('body');
+const info = document.getElementsByClassName('info');
+
+if (version === 'a') {
+  title.textContent = 'Version A';
+} else if (version === 'b') {
+  title.textContent = 'Version B';
+
+  image.src = 'public/hotel_room_b.png';
+
+  button.style.backgroundColor = '#86c9f0';
+
+  body.style.backgroundColor = '#ffffff';
+
+  title.style.color = '#c22d24';
+}
